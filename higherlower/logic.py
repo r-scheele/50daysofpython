12df924ece989c6f09026de30c38ed6e19a01dbc
Abdulrahman88-c <abdulrahmanolamilekan88@gmail.com>
2021-02-06 09:48:25 -0200

    code refactor

--- higherlower/logic.py
+++ higherlower/logic.py
@@ -1,45 +1,55 @@
 import os
 from art import logo,vs
-from helpers import test, compare
+from game_data import data
+import random
 
 
+def check_answer(guess, account_a, account_b):
+    """Takes a guess input and checks if it matches the answer."""
+    
+    if  account_a['follower_count'] > account_b['follower_count']:
+        return guess == 'A'
+    else:
+        return guess == 'B'
 
+def format_account(account):
+    name = account['name']
+    desc = account['description']
+    country = account['country']
 
+    return f"{name}, A {desc} from {country}"
 
 
 def game():
     game_on = True
     score = 0
 
-    first = compare()
-    second = compare()
+    first_account = random.choice(data)
+    second_account = random.choice(data)
+
+    if first_account == second_account:
+        second_account = random.choice(data)
 
     while game_on:
 
         print(logo)
 
-
         if score != 0:
             print(f"You're right. current score: {score}")
 
-        print(f"Compare A: {first['name']}, {first['description']}")
-
-
+        print(format_account(first_account))
         print(vs)
-        print(f"Against B: {second['name']}, {second['description']}")
+        print(format_account(second_account))
 
-        ans = input("Who has more followers? Type 'A' or 'B': ").upper()
+        guess = input("Who has more followers? Type 'A' or 'B': ").upper()
 
-        l = {
-            'A': first,
-            'B': second
-        }
-        result = test(ans, first, second)
+       
+        result = check_answer(guess, first_account, second_account)
 
-        if ans == result:
+        if result:
             score += 1
 
-            first, second = l[result], compare()
+            first_account, second_account = second_account, random.choice(data)
             os.system('clear')
         else:
             print(logo)
